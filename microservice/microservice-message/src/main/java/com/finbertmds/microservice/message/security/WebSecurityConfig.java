56d0dafb761093298b8c5a21d7f75a4fe7afc38d
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2020-10-31 22:36:23 +0900

    micro-message: fix web security for web

--- microservice/microservice-message/src/main/java/com/finbertmds/microservice/message/security/WebSecurityConfig.java
+++ microservice/microservice-message/src/main/java/com/finbertmds/microservice/message/security/WebSecurityConfig.java
@@ -6,8 +6,8 @@ import com.finbertmds.microservice.message.security.jwt.AuthTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,59 +21,131 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-	@Autowired
-	private UserDetailsService userDetailsService;
+	@Configuration
+	@Order(1)
+	public static class RestApiSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private AuthEntryPointJwt unauthorizedHandler;
+		@Autowired
+		private UserDetailsService userDetailsService;
 
-	@Bean
-	public AuthTokenFilter authenticationJwtTokenFilter() {
-		return new AuthTokenFilter();
+		@Autowired
+		public void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+		}
+
+		@Bean
+		public BCryptPasswordEncoder bCryptPasswordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
+
+		@Autowired
+		private AuthEntryPointJwt unauthorizedHandler;
+
+		@Bean
+		public AuthTokenFilter authenticationJwtTokenFilter() {
+			return new AuthTokenFilter();
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.csrf().disable()
+				.antMatcher("/api/**")
+				.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.authorizeRequests()
+					.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+					.antMatchers("/api/**").hasAnyRole("USER")
+					.anyRequest().authenticated();
+			// @formatter:on
+			http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+		}
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// @formatter:off
-		http
-			.csrf().disable()
-			.formLogin()
-				.loginProcessingUrl("/login")
-				.loginPage("/")
-				.defaultSuccessUrl("/chat")
-				.and()
-			.logout()
-				.logoutSuccessUrl("/")
-				.and()
-			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.authorizeRequests()
-				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-				.antMatchers("/api/**").hasAnyRole("USER")
-				.antMatchers("/**/ws/**").permitAll()
-				.antMatchers("/**/health/**").permitAll()
-				.antMatchers("/login", "/new-account", "/").permitAll()
-				.antMatchers(HttpMethod.POST, "/chatroom").hasRole("ADMIN")
-				.anyRequest().permitAll();
-		// @formatter:on
-		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-	}
+	@Configuration
+	@Order(2)
+	public static class LoginFormSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
+		@Autowired
+		private UserDetailsService userDetailsService;
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-	}
+		@Autowired
+		public void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+		}
 
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
+		@Bean
+		public BCryptPasswordEncoder bCryptPasswordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+				.csrf().disable()
+				.formLogin()
+					.loginProcessingUrl("/login")
+					.loginPage("/")
+					.defaultSuccessUrl("/chat")
+					.and()
+				.logout()
+					.logoutSuccessUrl("/")
+					.and()
+				.authorizeRequests()
+					.antMatchers("/webjars/**").permitAll()
+					.antMatchers("/**/ws/**").permitAll()
+					.antMatchers("/**/health/**").permitAll()
+					.antMatchers("/login", "/new-account", "/").permitAll()
+					.antMatchers(HttpMethod.POST, "/chatroom").hasRole("ADMIN")
+					.anyRequest().authenticated();
+			// @formatter:on
+		}
 	}
+	// @Override
+	// protected void configure(HttpSecurity http) throws Exception {
+	// 	// @formatter:off
+	// 	http
+	// 		.csrf().disable()
+	// 		.formLogin()
+	// 			.loginProcessingUrl("/login")
+	// 			.loginPage("/")
+	// 			.defaultSuccessUrl("/chat")
+	// 			.and()
+	// 		.logout()
+	// 			.logoutSuccessUrl("/")
+	// 			.and()
+	// 		.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+	// 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+	// 		.authorizeRequests()
+	// 			.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+	// 			.antMatchers("/api/**").hasAnyRole("USER")
+	// 			.antMatchers("/**/ws/**").permitAll()
+	// 			.antMatchers("/**/health/**").permitAll()
+	// 			.antMatchers("/login", "/new-account", "/").permitAll()
+	// 			.antMatchers(HttpMethod.POST, "/chatroom").hasRole("ADMIN")
+	// 			.anyRequest().authenticated();
+	// 	// @formatter:on
+	// http.addFilterBefore(authenticationJwtTokenFilter(),
+	// UsernamePasswordAuthenticationFilter.class);
+	// }
+
+	// @Bean
+	// @Override
+	// public AuthenticationManager authenticationManagerBean() throws Exception {
+	// return super.authenticationManagerBean();
+	// }
+
+	// @Autowired
+	// public void configure(AuthenticationManagerBuilder auth) throws Exception {
+	// auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	// }
+
+	// @Bean
+	// public BCryptPasswordEncoder bCryptPasswordEncoder() {
+	// return new BCryptPasswordEncoder();
+	// }
 }
