6f4171f43ad29740ff9beba7ae4397263d61c97f
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2020-11-07 12:25:43 +0900

    micro-security: init data by CommandLineRunner

--- microservice/microservice-security/src/main/java/com/finbertmds/microservice/security/SecurityApp.java
+++ microservice/microservice-security/src/main/java/com/finbertmds/microservice/security/SecurityApp.java
@@ -1,8 +1,14 @@
 package com.finbertmds.microservice.security;
 
+import com.finbertmds.microservice.security.models.ERole;
+import com.finbertmds.microservice.security.models.Role;
+import com.finbertmds.microservice.security.repository.RoleRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +20,21 @@ public class SecurityApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApp.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(RoleRepository roleRepository) {
+		return (args) -> {
+			if (!roleRepository.findByName(ERole.ROLE_USER).isPresent()) {
+				roleRepository.save(new Role(ERole.ROLE_USER));
+			}
+			if (!roleRepository.findByName(ERole.ROLE_MODERATOR).isPresent()) {
+				roleRepository.save(new Role(ERole.ROLE_MODERATOR));
+			}
+			if (!roleRepository.findByName(ERole.ROLE_ADMIN).isPresent()) {
+				roleRepository.save(new Role(ERole.ROLE_ADMIN));
+			}
+		};
 	}
 
 }
