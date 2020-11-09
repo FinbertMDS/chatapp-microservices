fa2603fbaade6e70cb2e98ed3a03968373c83ab5
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2020-11-09 17:05:15 +0900

    swagger add Authorization header jwt

--- microservice/microservice-message/src/main/java/com/finbertmds/microservice/message/configuration/SpringFoxConfig.java
+++ microservice/microservice-message/src/main/java/com/finbertmds/microservice/message/configuration/SpringFoxConfig.java
@@ -1,18 +1,58 @@
 package com.finbertmds.microservice.message.configuration;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
+@Import(springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig {
+
+  public static final String AUTHORIZATION_HEADER = "Authorization";
+
   @Bean
-  public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.ant("/api/**")).build();
-  }
+  public Docket swaggerSpringfoxDocket() {
+
+    Docket docket = new Docket(DocumentationType.SWAGGER_2).securityContexts(Arrays.asList(securityContext()))
+            .securitySchemes(Arrays.asList(apiKey()));
+
+        docket = docket.select()
+            .paths(PathSelectors.ant("/api/**"))
+            .build();
+        return docket;
+    }
+
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+            .securityReferences(defaultAuth())
+            .forPaths(PathSelectors.ant("/api/**"))
+            .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+            = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(
+            new SecurityReference("JWT", authorizationScopes));
+    }
 }
