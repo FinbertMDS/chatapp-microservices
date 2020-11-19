36dd73624cb811d3c42efee94cb901fc9f5aa521
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2020-11-19 13:41:20 +0900

    update format index.js

--- client/web/chatapp/src/index.js
+++ client/web/chatapp/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
+import reducer, { initialState } from './reducer';
 import reportWebVitals from './reportWebVitals';
 import { StateProvider } from './StateProvider';
-import reducer, { initialState } from './reducer';
 
 ReactDOM.render(
   <React.StrictMode>
