3739aae1a0fcb21942f7c96ec662fe4bfe8a7c17
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2020-11-27 11:25:31 +0900

    update script: add check container is running

--- scripts/wait-for-services.sh
+++ scripts/wait-for-services.sh
@@ -5,6 +5,7 @@ done=false
 host=127.0.0.1
 port=8080
 serviceName=$1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 main() {
 	declare -a validate
@@ -27,7 +28,22 @@ main() {
 		#curl -q http://${1?}:8080/health >& /dev/null && curl -q http://${1?}:8081/health >& /dev/null && curl -q http://${1?}:8082/health >& /dev/null
 		echo -n .
 		sleep 5
+		local statusContainer=$(checkContainerIsRunning $serviceName)
+		if [ "${statusContainer}" == "0" ]; then
+			echo "${serviceName} is stopped"
+			break;
+		fi
 	done
+}
+
+checkContainerIsRunning() {
+	cd $DIR/../docker
+	if [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q ${1})` ]; then
+  	echo 0
+	else
+		echo 1
+	fi
+	cd $DIR
 }
 
 checkStatus() {
